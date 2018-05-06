1c83363165a972e0b18868d77d8d805866e6de64
nwactor <nickwactor@gmail.com>
2018-05-05 17:44:54 -0700

    project area layout for new portfolio

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -1,9 +1,25 @@
-$('.nav a').on('click', event => {
-	event.preventDefault();
+$('.nav a').on('click', function(event) {
+	//When this callback used ES6 notation for function declaration, 'this' pointed to the window... 
+	// event.preventDefault();
 
-	$('.nav li').removeClass('active');
-	$(this).parent('li').addClass('active');
+	// $('.nav li').removeClass('active');
+	// $(this).parent('li').addClass('active');
+	
 
 	scrollSpyRefresh();
     waypointsRefresh();
 });
+
+/* refresh scrollspy */
+function scrollSpyRefresh() {
+    setTimeout(function () {
+	$('body').scrollspy('refresh');
+    }, 1000);
+}
+
+/* refresh waypoints */
+function waypointsRefresh() {
+    setTimeout(function () {
+	$.waypoints('refresh');
+    }, 1000);
+}
